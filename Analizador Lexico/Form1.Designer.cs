dfbec759323d134fee03d75294b85b05ab968258
Carlos <carloscelis404@gmail.com>
2024-11-04 08:37:48 -0700

    Agregar archivos de proyecto.

---
+++ Analizador Lexico/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Analizador_Lexico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.TxtText = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgvRes = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BtnSalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BtnSalir);
+            this.groupBox1.Controls.Add(this.BtnReset);
+            this.groupBox1.Controls.Add(this.BtnStart);
+            this.groupBox1.Controls.Add(this.TxtText);
+            this.groupBox1.Location = new System.Drawing.Point(15, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(634, 345);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos";
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(210)))), ((int)(((byte)(255)))));
+            this.BtnReset.Location = new System.Drawing.Point(141, 307);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(113, 31);
+            this.BtnReset.TabIndex = 2;
+            this.BtnReset.Text = "Reiniciar";
+            this.BtnReset.UseVisualStyleBackColor = false;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(132)))));
+            this.BtnStart.Location = new System.Drawing.Point(5, 307);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(113, 31);
+            this.BtnStart.TabIndex = 1;
+            this.BtnStart.Text = "Comenzar";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // TxtText
+            // 
+            this.TxtText.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtText.Location = new System.Drawing.Point(7, 28);
+            this.TxtText.Name = "TxtText";
+            this.TxtText.Size = new System.Drawing.Size(620, 257);
+            this.TxtText.TabIndex = 0;
+            this.TxtText.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgvRes);
+            this.groupBox2.Location = new System.Drawing.Point(656, 16);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(434, 351);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultados";
+            // 
+            // dgvRes
+            // 
+            this.dgvRes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvRes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvRes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvRes.Location = new System.Drawing.Point(6, 29);
+            this.dgvRes.Name = "dgvRes";
+            this.dgvRes.ReadOnly = true;
+            this.dgvRes.RowHeadersVisible = false;
+            this.dgvRes.RowHeadersWidth = 51;
+            this.dgvRes.RowTemplate.Height = 24;
+            this.dgvRes.Size = new System.Drawing.Size(422, 316);
+            this.dgvRes.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No.";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "No. Línea";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Cadena";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Tipo";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // BtnSalir
+            // 
+            this.BtnSalir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(186)))), ((int)(((byte)(186)))));
+            this.BtnSalir.Location = new System.Drawing.Point(277, 307);
+            this.BtnSalir.Name = "BtnSalir";
+            this.BtnSalir.Size = new System.Drawing.Size(113, 31);
+            this.BtnSalir.TabIndex = 3;
+            this.BtnSalir.Text = "Salir";
+            this.BtnSalir.UseVisualStyleBackColor = false;
+            this.BtnSalir.Click += new System.EventHandler(this.BtnSalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(209)))), ((int)(((byte)(184)))));
+            this.label1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label1.Location = new System.Drawing.Point(0, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Carlos Omar Celis Calzada";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(250)))), ((int)(((byte)(222)))));
+            this.ClientSize = new System.Drawing.Size(1102, 399);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Analizador Lexico";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox TxtText;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgvRes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Button BtnSalir;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Analizador Lexico/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Analizador_Lexico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[,] tablaTrans =
+        {
+            {12 ,2, 12, 12, 12, 12, 12 , 12}, //q0
+            {1 ,1, 1, 1, 1, 1, 1 , 1}, //q1
+            {1 ,2, 5, 3, 3, 7, 12, 12 }, //q2
+            {1 ,4, 1, 12, 12, 1, 12, 12 }, //q3
+            {1 ,4, 10, 13, 13, 7, 12, 12 }, //q4
+            {1 ,6, 1, 12, 12, 1, 12 , 12}, //q5
+            {1 ,6, 1, 8, 8, 1, 12, 12 }, //q6
+            {1 ,1, 1, 12, 12, 1 , 12, 12}, //q7
+            {1 ,9, 1, 12, 12, 1, 12, 12 }, //q8
+            {1 ,9, 1, 1, 1, 1, 12, 12 }, //q9
+            {1 ,11, 1, 1, 1, 1, 1, 1 }, //q10
+            {1 ,11, 1, 12, 12, 1, 12, 12 }, //q11
+            {12 ,12, 12, 12, 12, 12, 12, 12 }, //q12
+            {13 ,13, 13, 13, 13, 13, 12, 12 }, //q13
+        };
+        int EDO = 0;
+        int count = 0;
+        int tamCad = 0;
+        char[] let = {
+            'A', 'B', 'C', 'D', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        };
+        char[] nums = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        char porcentaje = '%';
+        char E = 'E';
+        char coma = ',';
+        char punto = '.';
+        char enter = '\n';
+        char espacio = ' ';
+        int lineCount = 1;
+        List<int> historialEstados = new List<int>();
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            EDO = 0;
+            count = 0;
+            tamCad = 0;
+            lineCount = 1;
+            historialEstados.Clear();
+            dgvRes.Rows.Clear();
+            anteriorCarac = '\0';
+
+            string textoTotal = TxtText.Text + " ";
+            tamCad = textoTotal.Length;
+            Automata(tablaTrans, ref EDO, count, tamCad, textoTotal);
+
+            int filaNum = 1;
+            foreach(DataGridViewRow row in dgvRes.Rows)
+            {
+                row.Cells[0].Value = filaNum;
+                filaNum++;
+            }
+        }
+        public void agregarFila(int lineaCoun, string cadeF, string tipo)
+        {
+            dgvRes.Rows.Add(null, lineaCoun, cadeF, tipo);
+        }
+
+        public void Automata(int[,] TT, ref int edo, int cont, int tamCad, string cad)
+        {
+            List<char> cadena = new List<char>();
+            while (cont < tamCad)
+            {
+                char car = cad[cont];
+                int carPointer = CaracterCheck(car);
+                edo = TT[edo, carPointer];
+                ManejarCadena(ref cadena, edo, car);
+                cont++;
+            }
+        }
+        char anteriorCarac;
+        public void ManejarCadena (ref List<char> cadena, int estado, char car) 
+        {
+            if (car == '\n')
+                lineCount++;
+            if (estado != 1 && estado != 12)
+            {
+                cadena.Add(car);
+            }
+            if (estado == 12)
+            {
+                string cadenaF = "";
+                CheckPuntosFinales(ref cadena, car);
+                foreach (char caract in cadena)
+                {
+                    cadenaF += caract;
+                }
+                if (cadenaF != "")
+                    Aceptados(historialEstados.Last(), cadenaF, lineCount);
+                cadena.Clear();
+                EDO = 0;
+            }
+            if (estado == 1)
+            {
+                string cadenaF = "";
+                CheckPuntosFinales(ref cadena, car);
+                cadena.Add(car);
+                foreach (char caract in cadena)
+                {
+                    cadenaF += caract;
+                }
+                if (cadenaF != "")
+                    Aceptados(1, cadenaF, lineCount);
+                cadena.Clear();
+                EDO = 0;
+            }
+            anteriorCarac = car;
+            historialEstados.Add(EDO);
+        }
+        public void CheckPuntosFinales(ref List<char> cadena, char car)
+        {
+            if (anteriorCarac == '.' && car == ' ' && cadena.Count > 0)
+            {
+                historialEstados.RemoveAt(historialEstados.Count - 1);
+                cadena.RemoveAt(cadena.Count-1);
+            }
+            if (anteriorCarac == ',' && car == ' ' && cadena.Count > 0)
+            {
+                historialEstados.RemoveAt(historialEstados.Count - 1);
+                cadena.RemoveAt(cadena.Count-1);
+            }
+        }
+        public void Aceptados(int estado, string cadenaF, int lineaCount)
+        {
+            switch (estado)
+            {
+                case 2:
+                    agregarFila(lineaCount, cadenaF, "Natural");
+                    break;
+                case 4:
+                    agregarFila(lineaCount, cadenaF, "Real");
+                    break;
+                case 6:
+                    agregarFila(lineaCount, cadenaF, "Exponencial");
+                    break;
+                case 9:
+                    agregarFila(lineaCount, cadenaF, "Exponencial");
+                    break;
+                case 11:
+                    agregarFila(lineaCount, cadenaF, "Exponencial");
+                    break;
+                case 7:
+                    agregarFila(lineaCount, cadenaF, "Porcentual");
+                    break;
+                default:
+                    agregarFila(lineaCount, cadenaF, "Invalida");
+                    break;
+            }
+        }
+        public int CaracterCheck (char cara)
+        {
+            if (let.Contains(cara)) return 0;
+            if (nums.Contains(cara)) return 1;
+            if (E == cara) return 2;
+            if (coma == cara) return 3;
+            if (punto == cara) return 4;
+            if (porcentaje == cara) return 5;
+            if (espacio == cara) return 6;
+            if (enter == cara) return 7;
+            return 6;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            ResetearSistema();
+        }
+        private void ResetearSistema()
+        {
+            EDO = 0;
+            count = 0;
+            tamCad = 0;
+            lineCount = 1;
+
+            historialEstados.Clear();
+
+            dgvRes.Rows.Clear();
+
+            TxtText.Clear();
+
+            anteriorCarac = '\0';
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
